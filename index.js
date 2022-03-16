9d02ab1670c2429793430deb3d8dbf4a295417c3
MohammadRakib1e18 <trustedrakib@gmail.com>
2022-03-16 14:35:15 +0600

    added

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
+const cors = require("cors");
+const ObjectId = require("mongodb").ObjectId;
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.np0as.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db("story");
+        const serviceCollection = database.collection("products");
+
+        //GET Api
+        app.get("/services", async (req, res) => {
+            const cursor = serviceCollection.find({});
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+
+        // // DELETE API
+        // app.delete("/deleteOrder", async (req, res) => {
+        //     const id = req.params.id;
+        //     const query = { _id: ObjectId(id) };
+        //     const result = await orderCollection.deleteOne(query);
+
+        //     console.log("deleting user with id ", result);
+
+        //     res.json(result);
+        // });
+
+    } finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+    res.send("Server is running");
+});
+
+app.listen(port, () => {
+    console.log("Server is running at port", port);
+});
